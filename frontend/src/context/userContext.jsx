058758f9527a44119bf9d2572dfdc2951169e96e
Anas Usman <145061455+anas-6380@users.noreply.github.com>
2025-08-04 13:02:58 +0530

    Update userContext.jsx
    socket io added extra in user context

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -3,7 +3,10 @@ import { authDataContext } from './AuthContext'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 export const userDataContext=createContext()
+import{io}from "socket.io-client"
 
+
+export let socket=io("https://linkedin-backend-mn3h.onrender.com")
 function UserContext({children}) {
 let [userData,setUserData]=useState(null)
 let {serverUrl}=useContext(authDataContext)
